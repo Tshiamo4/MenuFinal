40862bb57cf3851ca89878cc53dec256eba7a1d4
Tshiamo4 <ST10471254@imconnect.edu.za>
2025-11-13 14:44:28 +0200

    update

--- App.tsx
+++ App.tsx
@@ -691,11 +691,3 @@ const styles = StyleSheet.create({
 
 
 
-
-
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Tshiamo4/MenuFinal.git
-git push -u origin main
